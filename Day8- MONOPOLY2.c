7f0f1d681cbd64c7a5848fd69e707c9af4d093c8
Pradunya-k <pradunyk2405@gmail.com>
2024-10-05 22:47:37 +0530

    Add files via upload

---
+++ Day8- MONOPOLY2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int T, P, Q, R, S;
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d %d %d %d", &P, &Q, &R, &S);
+        if (P > Q + R + S || Q > P + R + S || R > P + Q + S || S > P + Q + R) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
