62a07d523ac33e13005b16aad469c351b4235803
Pradunya-k <pradunyk2405@gmail.com>
2024-10-24 21:49:36 +0530

    Add files via upload

---
+++ Day22-DPOLY.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+        int degree = -1;
+        for (int i = 0; i < N; i++) {
+            int coefficient;
+            scanf("%d", &coefficient);
+            if (coefficient != 0) {
+                degree = i;
+            }
+        }
+        printf("%d\n", degree);
+    }
+    return 0;
+}
