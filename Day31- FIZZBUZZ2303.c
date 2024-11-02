ed662d9eb3ef47af84446b27820bbc2c8e0774d4
Pradunya-k <pradunyk2405@gmail.com>
2024-11-02 09:13:16 +0530

    Add files via upload

---
+++ Day31- FIZZBUZZ2303.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int T, N;
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d", &N);
+        int choices = N * (N - 1);
+        printf("%d\n", choices);
+    }
+    return 0;
+}
